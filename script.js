eaef3d3bd792d5c953c945e648b7d7816c2d4f92
SageM <madduxsage@gmail.com>
2023-09-12 15:02:50 -0400

    Commit with the buying a cat conditional statment/switch statment/ harry potter array

--- script.js
+++ script.js
@@ -21,3 +21,48 @@
 // } else {
 //     console.log(`You cannot buy a cat. You need ${dollarsLeftPositive} more dollars :(`)
 // }
+
+// const ageInYears = 19
+// ageInYears > 18 ? console.log('can vote') : console.log('can not vote')
+
+// const food = 'fish';
+
+// switch(food) {
+//   case 'chicken':
+//     console.log('Ill have the Chicken Parmesean');
+//     break;
+//   case 'steak':
+//     console.log('The steak please, medium rare');
+//     break;
+//   case 'fish':
+//     console.log('I think Ill try the Salmon');
+//     break;
+//   default:
+//     console.log('please enter a valid entree');
+// }
+
+             
+            
+const wizards = [
+    {name: "Harry Potter", house: "Gryffindor"}, 
+    {name: "Lord Voldomort", house: "Slytherin"}, 
+    {name: "Cedric Diggory", house: "Hufflepuff"},
+    {name: "Luna Lovegood", house: "Ravenclaw"},  
+    {name: "Albus Dumbledor", house: "Gryffindor"}, 
+    {name: "Merlin", house: "Slytherin"}, 
+    {name: "Pomona Sprout", house: "Hufflepuff"}, 
+    {name: "Gilderoy Lockheart", house: "Ravenclaw"}, 
+    {name: "Ron Weasley", house: "Gryffindor"}, 
+    {name: "Severus Snape", house: "Slytherin"}, 
+    {name: "Nymphadora Tonks", house: "Hufflepuff"}, 
+    {name: "Padma Patil", house: "Ravenclaw"}, 
+    {name: "Hermoine Granger", house: "Gryffindor"} 
+   ]
+
+   for(let i = 0; i < wizards.length; i++){
+    if(wizards[i].house == "Slytherin") {
+        console.log(wizards[i].name)
+    }
+   
+}  
+
