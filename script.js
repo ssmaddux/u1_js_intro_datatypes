ba2760b82fa0d0e1edb68cd09906853d38914256
SageM <madduxsage@gmail.com>
2023-09-12 13:50:51 -0400

    idk

---
+++ script.js
@@ -0,0 +1,23 @@
+// let name = "Jeremy"
+// let age = 36
+// console.log(`hello my name is ${name} and I am ${age} years old`)
+
+// let friends = ["hay", "jay", "kay"]
+// console.log(friends[0])
+
+// let string1 = "Sage"
+// console.log(string1[0])
+
+// const yourMoney = 50
+// const catPrice = 100
+
+// dollarsLeft = yourMoney - catPrice
+// dollarsLeftPositive = Math.abs(dollarsLeft)
+
+// if(yourMoney == catPrice) {
+//     console.log(`You have just enough to buy a cat!`)
+// } else if (yourMoney > catPrice) {
+//     console.log(`You can buy a cat and will have ${dollarsLeft}$ left over`)
+// } else {
+//     console.log(`You cannot buy a cat. You need ${dollarsLeftPositive} more dollars :(`)
+// }
